3c445533f2cdc20016fa4206c0324123e75e8b46
Turbobot <turbobot@vercel.com>
2024-10-18 03:35:36 +0900

    feat(create-turbo): apply official-starter transform

--- apps/member/app/page.tsx
+++ apps/member/app/page.tsx
@@ -1,13 +1,14 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { Button } from 'shared/ui/button';
+import { HomePage } from 'pages/home';
 
 export default function Home() {
-  const router = useRouter();
-
   return (
     <div className=''>
-      <button onClick={() => router.push('/docs')}>MAIN</button>
+      <HomePage />
+      <Button></Button>
+      <button>MAIN</button>
     </div>
   );
 }
