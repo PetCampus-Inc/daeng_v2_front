b586c9b7fdc5359c65db4425313480c06c24a527
yeomgahui <ykh4933@naver.com>
2024-10-23 23:36:09 +0900

    feat(storybook): 스토리북 초기 설정 파일 설치

---
+++ apps/storybook/.storybook/preview.ts
@@ -0,0 +1,16 @@
+import type { Preview } from '@storybook/react';
+import '@knockdog/ui/globals.css';
+
+/** @type { import('@storybook/react').Preview } */
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
