33d1e4d58758cd6b23690b12d3b57ab6ec95184f
jsn0509 <jsn_@naver.com>
2024-10-25 01:21:11 +0900

    feat(ui): icon 스토리북 추가

---
+++ packages/ui/src/components/icon/icon.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Icon, IconProps, iconTypes } from '.';
+
+const meta: Meta<IconProps> = {
+  title: 'Components/Icon',
+  component: Icon,
+  tags: ['autodocs'],
+  parameters: { layout: 'centered' },
+  argTypes: {
+    icon: {
+      control: { type: 'select' },
+      options: iconTypes,
+    },
+  },
+  args: {
+    icon: iconTypes[0],
+  },
+};
+
+export default meta;
+type Story = StoryObj<IconProps>;
+
+export const Default: Story = {};
