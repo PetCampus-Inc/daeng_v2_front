18d84bae5cbbd14bee034269210a07142c8f7623
jsn0509 <jsn_@naver.com>
2024-11-10 15:17:50 +0900

    feat(ui): 검색, 눈, 클로즈 아이콘 추가

---
+++ packages/ui/src/components/icon/icons/Search.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgSearch = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+    {...props}
+  >
+    <path
+      fillRule='evenodd'
+      clipRule='evenodd'
+      d='M11 15C8.79086 15 7 13.2091 7 11C7 8.79086 8.79086 7 11 7C13.2091 7 15 8.79086 15 11C15 13.2091 13.2091 15 11 15ZM11 17C7.68629 17 5 14.3137 5 11C5 7.68629 7.68629 5 11 5C14.3137 5 17 7.68629 17 11C17 12.3602 16.5474 13.6147 15.7845 14.6211L18.524 17.3606C18.9145 17.7511 18.9145 18.3843 18.524 18.7748C18.1334 19.1653 17.5003 19.1653 17.1098 18.7748L14.3281 15.9931C13.3757 16.6292 12.2312 17 11 17Z'
+      fill='#B5B5B5'
+    />
+  </svg>
+);
+export default SvgSearch;
