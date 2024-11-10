1404ba36fc0847a1cf27c9bfb1c30c5aa7abaa48
jsn0509 <jsn_@naver.com>
2024-11-10 15:19:58 +0900

    feat(ui): 검색 인풋 컴포넌트 추가

---
+++ packages/ui/src/components/input/ui/SearchInput.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { IconButton } from '../../icon-button';
+import { Input, type InputProps } from './Input';
+
+export interface SearchInputProps extends InputProps {
+  onClear?: () => void;
+}
+
+export const SearchInput = ({ value, onClear, ...props }: SearchInputProps) => {
+  return (
+    <Input
+      type='search'
+      inputMode='search'
+      value={value}
+      suffixElement={
+        value ? (
+          <IconButton
+            className='mx-1'
+            icon='CircleCloseGray'
+            pressEffect
+            onClick={onClear}
+          />
+        ) : (
+          <IconButton className='mx-1' icon='Search' />
+        )
+      }
+      {...props}
+    />
+  );
+};
