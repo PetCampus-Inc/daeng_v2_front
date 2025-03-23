7bf6a95ef4501b7d31a4294f89aa07b55066b924
jsn0509 <jsn_@naver.com>
2025-03-24 05:35:53 +0900

    feat: 공용 페이지 스캐폴딩 파일 생성

---
+++ apps/knockdog/app/stack/notification/page.tsx
@@ -0,0 +1,5 @@
+/** [공용/스택] 알림 페이지 */
+
+export default function NotificationPage() {
+  return <div>NotificationPage</div>;
+}
---
+++ apps/knockdog/app/stack/setting/page.tsx
@@ -0,0 +1,5 @@
+/** [공용/스택] 설정 페이지 */
+
+export default function SettingPage() {
+  return <div>SettingPage</div>;
+}
