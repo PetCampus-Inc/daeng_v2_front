e84e2d4e3e7f19b473cef5795593e7e505f56661
im-na0 <139189221+im-na0@users.noreply.github.com>
2025-04-29 19:13:18 +0900

    build(knockdog): next standalone 설정 활성화

--- apps/knockdog/next.config.js
+++ apps/knockdog/next.config.js
@@ -1,3 +1,10 @@
-const nextConfig = {};
+const path = require('path');
+
+const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
+};
 
 module.exports = nextConfig;
