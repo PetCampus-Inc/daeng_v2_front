03d9b9121494b648bae26bd20e169fdb9ecc7cd4
yeomgahui <ykh4933@naver.com>
2025-05-13 11:10:11 +0900

    ci(config): dockefile 정적파일 제거, assetPrefix 추가

--- apps/knockdog/next.config.js
+++ apps/knockdog/next.config.js
@@ -1,7 +1,15 @@
 const path = require('path');
 
+const isProduction = process.env.NODE_ENV === 'production';
+const assetPrefix = isProduction ? process.env.NEXT_PUBLIC_ASSET_PREFIX : '';
+
 const nextConfig = {
   output: 'standalone',
+  assetPrefix,
+  images: {
+    loader: 'default',
+    path: assetPrefix,
+  },
   experimental: {
     outputFileTracingRoot: path.join(__dirname, '../../'),
   },
