d8da5be85222947685ccc78032bec1c6b7b0278a
im-na0 <139189221+im-na0@users.noreply.github.com>
2025-05-14 22:03:50 +0900

    chore(config): migrate to ESLint v9 and flat config

--- apps/storybook/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ['@knockdog/eslint-config/next.js'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: true,
-  },
-};
