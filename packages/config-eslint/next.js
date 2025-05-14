af660a0760e58bf315cab750f0fe3023298be75e
im-na0 <139189221+im-na0@users.noreply.github.com>
2025-05-14 23:08:50 +0900

    chore(config): migrate to ESLint v9 and flat config (#25)
    * chore(config): migrate to ESLint v9 and flat config
    
    * fix(config): eslint config 수정

--- packages/config-eslint/next.js
+++ packages/config-eslint/next.js
@@ -1,53 +1,69 @@
-const { resolve } = require('node:path');
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import tseslint from 'typescript-eslint';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginReact from 'eslint-plugin-react';
+import globals from 'globals';
+import pluginNext from '@next/eslint-plugin-next';
+import { config as baseConfig } from './base.js';
 
-const project = resolve(process.cwd(), 'tsconfig.json');
-
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  extends: [
-    'next',
-    'turbo',
-    'eslint:recommended',
-    'prettier',
-    'airbnb',
-    'airbnb-typescript',
-    'next/core-web-vitals',
-    '@feature-sliced',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  globals: {
-    React: true,
-    JSX: true,
-  },
-  env: {
-    node: true,
-    browser: true,
-  },
-  plugins: ['only-warn', '@typescript-eslint'],
-  settings: {
-    'import/resolver': {
-      typescript: {
-        project,
+/**
+ * A custom ESLint configuration for libraries that use Next.js.
+ *
+ * @type {import("eslint").Linter.Config[]}
+ * */
+export const nextJsConfig = [
+  ...baseConfig,
+  js.configs.recommended,
+  eslintConfigPrettier,
+  importPlugin.flatConfigs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    languageOptions: {
+      ...pluginReact.configs.flat.recommended.languageOptions,
+      globals: {
+        ...globals.serviceworker,
       },
     },
   },
-  ignorePatterns: ['.*.js', 'node_modules/', 'next.config.js'],
-  overrides: [{ files: ['*.js?(x)', '*.ts?(x)'] }],
-  rules: {
-    '@typescript-eslint/no-empty-function': 'error',
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
-    'no-unused-vars': 'error',
-    'no-debugger': 'error',
-    eqeqeq: ['error', 'always'],
-    curly: ['error', 'multi-line'],
-    camelcase: ['error', { properties: 'always' }],
-    'id-length': [
-      'error',
-      { min: 2, max: 30, exceptions: ['_', 'i', 'j', 'x', 'y'] },
-    ],
-    'import/prefer-default-export': 'off',
-    'import/no-internal-modules': 'off',
-    'import/no-extraneous-dependencies': 'off',
+  {
+    plugins: {
+      '@next/next': pluginNext,
+    },
+    rules: {
+      ...pluginNext.configs.recommended.rules,
+      ...pluginNext.configs['core-web-vitals'].rules,
+    },
   },
-};
+  {
+    plugins: {
+      'react-hooks': pluginReactHooks,
+    },
+    settings: { react: { version: 'detect' } },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      // React scope no longer necessary with new JSX transform.
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-empty-function': 'error',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-unused-vars': 'error',
+      'no-debugger': 'error',
+      eqeqeq: ['error', 'always'],
+      curly: ['error', 'multi-line'],
+      camelcase: ['error', { properties: 'always' }],
+      'id-length': [
+        'error',
+        { min: 2, max: 30, exceptions: ['_', 'i', 'j', 'x', 'y'] },
+      ],
+      'import/prefer-default-export': 'off',
+      'import/no-internal-modules': 'off',
+      'import/no-extraneous-dependencies': 'off',
+    },
+  },
+];
