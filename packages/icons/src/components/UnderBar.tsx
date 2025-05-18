1a2d46763e668bdf3360c3d7706e4574fa9a74e6
SouthNova <jsn950509@gmail.com>
2025-05-19 02:33:30 +0900

    feat(icons): 아이콘 SVG 컴포넌트들 추가

---
+++ packages/icons/src/components/UnderBar.tsx
@@ -0,0 +1,17 @@
+export function UnderBar(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <path
+        d='M18.1025 19.0049C18.6067 19.0562 19 19.4823 19 20C19 20.5177 18.6067 20.9438 18.1025 20.9951L18 21H6C5.44772 21 5 20.5523 5 20C5 19.4477 5.44772 19 6 19H18L18.1025 19.0049ZM8 12C8 14.2091 9.79086 16 12 16C14.2091 16 16 14.2091 16 12V5H18V12L17.9922 12.3086C17.8316 15.4789 15.2102 18 12 18C8.78979 18 6.16843 15.4789 6.00781 12.3086L6 12V5H8V12Z'
+        fill='currentColor'
+      />
+    </svg>
+  );
+}
---
+++ packages/icons/src/components/Won.tsx
@@ -0,0 +1,18 @@
+export function Won(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <path
+        d='M19.875 5L16.9715 19H13.8138L11.973 7.71404L10.1862 19H7.02855L4.125 5H7.11326L8.69211 14.9333L10.6175 5H13.4749L15.3002 14.9333L16.9638 5H19.875Z'
+        fill='currentColor'
+      />
+      <rect x='3' y='10.3846' width='18' height='2.46154' fill='currentColor' />
+    </svg>
+  );
+}
