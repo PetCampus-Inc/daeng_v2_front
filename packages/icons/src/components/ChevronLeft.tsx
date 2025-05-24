ca90f6c2fd95451f1e88410568ba192e63d6e12f
yeomgahui <ykh4933@naver.com>
2025-05-24 23:50:20 +0900

    feat(icons): 아이콘 컴포넌트 추가: Chevron 시리즈 및 CompareNav, MypageNav, SaveNav, SearchNav 컴포넌트 생성

---
+++ packages/icons/src/components/ChevronLeft.tsx
@@ -0,0 +1,19 @@
+export function ChevronLeft(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M9.53656 12L15.6508 6.75927L14.3492 5.24075L7.34918 11.2408L6.46338 12L7.34918 12.7593L14.3492 18.7593L15.6508 17.2408L9.53656 12Z'
+        fill='currentColor'
+      />
+    </svg>
+  );
+}
