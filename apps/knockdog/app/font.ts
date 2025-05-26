f99e96c84d96363c73f0adac44bb0f0a60b169ed
im-na0 <139189221+im-na0@users.noreply.github.com>
2025-05-26 18:36:22 +0900

    refactor(knockdog): replace Pretendard with SUIT font and clean up font assets

---
+++ apps/knockdog/app/font.ts
@@ -0,0 +1,8 @@
+import localFont from 'next/font/local';
+
+export const suit = localFont({
+  src: '../public/fonts/SUIT-Variable.woff2',
+  weight: '100 900',
+  variable: '--font-suit',
+  display: 'swap',
+});
