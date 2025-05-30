d479157cb6002336e4e0a7b8d53c9bb8d5bf66eb
SouthNova <jsn950509@gmail.com>
2025-05-31 02:38:33 +0900

    fix(naver-map): 제거되지 않은 export 제거

--- packages/naver-map/src/components/index.ts
+++ packages/naver-map/src/components/index.ts
@@ -2,5 +2,4 @@ export { NaverMap } from './NaverMap';
 export { NaverMapCore } from './NaverMapCore';
 
 export { Overlay } from './Overlay';
-export { Marker } from './Marker';
 export { CurrentLocationMarker } from './CurrentLocationMarker';
