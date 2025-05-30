b730c119241caf77faa16b40b52b2186799b5c22
SouthNova <jsn950509@gmail.com>
2025-05-31 02:32:34 +0900

    feat(naver-map): 현재 위치 표시해 주는CurrentLocationMarker 컴포넌트 추가

--- packages/naver-map/src/components/index.ts
+++ packages/naver-map/src/components/index.ts
@@ -2,3 +2,5 @@ export { NaverMap } from './NaverMap';
 export { NaverMapCore } from './NaverMapCore';
 
 export { Overlay } from './Overlay';
+export { Marker } from './Marker';
+export { CurrentLocationMarker } from './CurrentLocationMarker';
