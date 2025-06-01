39548d8c85d4d59558f8b5865473f0485f0661e2
yeomgahui <ykh4933@naver.com>
2025-06-01 21:57:17 +0900

    feat(ui,icons): 아이콘 메타데이터 업데이트 및 새로운 아이콘 컴포넌트 추가

---
+++ packages/icons/src/components/CheckFill.tsx
@@ -0,0 +1,17 @@
+export function CheckFill(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <path
+        d='M12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3ZM10.7363 14.3203L7.85254 11.3662L7 12.1904L10.7178 16L17 9.84277L16.166 9L10.7363 14.3203Z'
+        fill='currentColor'
+      />
+    </svg>
+  );
+}
--- packages/icons/src/components/Naver.tsx
+++ packages/icons/src/components/Naver.tsx
@@ -10,7 +10,7 @@ export function Naver(props: React.SVGProps<SVGSVGElement>) {
     >
       <path
         d='M9.35915 19.5H4.5V4.5H9.35915L14.8521 12.4787V4.5H19.5V19.5H14.8521L9.35915 11.6277V19.5Z'
-        fill='#03C75A'
+        fill='currentColor'
       />
     </svg>
   );
