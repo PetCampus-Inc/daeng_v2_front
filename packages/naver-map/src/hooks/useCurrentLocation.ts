95ee70a3b31616440855549c89d0586610d5127d
SouthNova <jsn950509@gmail.com>
2025-06-05 14:25:46 +0900

    refactor(naver-map): useCurrentLocation에 naver.maps 의존 제거

--- packages/naver-map/src/hooks/useCurrentLocation.ts
+++ packages/naver-map/src/hooks/useCurrentLocation.ts
@@ -2,25 +2,18 @@
 
 import { useEffect, useState } from 'react';
 
+type Location = { lat: number; lng: number };
+
 export function useCurrentLocation() {
-  const [position, setPosition] = useState<naver.maps.LatLng | undefined>(
-    undefined
-  );
+  const [position, setPosition] = useState<Location | undefined>(undefined);
 
   useEffect(() => {
-    if (typeof naver === 'undefined')
-      throw new Error(
-        '네이버 지도 인스턴스가 초기화되지 않았습니다.\n> NaverMap 컴포넌트 내부에서 사용해 주세요.'
-      );
-
     try {
       window.navigator.geolocation.getCurrentPosition((position) => {
-        setPosition(
-          new naver.maps.LatLng(
-            position.coords.latitude,
-            position.coords.longitude
-          )
-        );
+        setPosition({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
       });
     } catch (error) {
       console.error(error);
