7b2f876e821ecd227c309192403c4f58a9fc71d9
im-na0 <139189221+im-na0@users.noreply.github.com>
2025-06-20 22:59:30 +0900

    feat(knockdog): device 관련 utils 추가
    현재 환경이 client인지 sever인지 판별하는 유틸함수 추가 (isClient, isSever)

--- apps/knockdog/src/shared/lib/index.ts
+++ apps/knockdog/src/shared/lib/index.ts
@@ -1,5 +1,7 @@
+export * from './device';
 export * from './dom';
 export * from './react';
+export * from './storage';
 export * from './utils';
 export * from './embla';
 export * from './geolocation';
