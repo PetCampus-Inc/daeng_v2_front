0d3cfb1e6580458c71489a28a1e377163803f56b
im-na0 <139189221+im-na0@users.noreply.github.com>
2025-06-21 18:34:52 +0900

    refactor(design-token): 디자인 토큰 패키지 구조 변경

---
+++ packages/design-token/vars/index.mjs
@@ -0,0 +1,4 @@
+export * as $dimension from './dimension.mjs';
+export * as $color from './color.mjs';
+export * as $radius from './radius.mjs';
+export * as $opacity from './opacity.mjs';
---
+++ packages/design-token/vars/radius.mjs
@@ -0,0 +1,6 @@
+export const r1 = 'var(--radius-r1)';
+export const r2 = 'var(--radius-r2)';
+export const r3 = 'var(--radius-r3)';
+export const r4 = 'var(--radius-r4)';
+export const r5 = 'var(--radius-r5)';
+export const full = 'var(--radius-full)';
