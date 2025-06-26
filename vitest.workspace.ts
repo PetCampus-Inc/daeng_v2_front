4da76fa1433d3e64801ee1cad1e131897832a6d3
yeomgahui <ykh4933@naver.com>
2025-06-26 13:58:39 +0900

    chore(root): 테스팅 라이브러리 추가

---
+++ vitest.workspace.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    name: 'headless-ui',
+    include: ['./packages/headless-ui/**/*.test.(ts|tsx)'],
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/build/**',
+      '**/coverage/**',
+    ],
+    environment: 'jsdom',
+  },
+});
