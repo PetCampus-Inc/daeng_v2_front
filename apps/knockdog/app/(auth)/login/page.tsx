e017d83012997666cefc742ed04540ccc6a48b6f
yeomgahui <ykh4933@naver.com>
2025-07-01 10:47:20 +0900

    feat(auth): 앱 진입 퍼블리싱 routing 수정

--- apps/knockdog/app/(auth)/login/page.tsx
+++ apps/knockdog/app/(auth)/login/page.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import { Icon, Divider } from '@knockdog/ui';
 
-export default function IntroPage() {
+export default function LoginPage() {
   return (
     <div>
       {/* 하단 영역 */}
       <div className='fixed bottom-[78px] left-0 right-0 flex flex-col gap-y-3 bg-white px-4'>
-        <button className='body1-bold text-text-primary flex w-full items-center justify-center gap-x-2 rounded-lg bg-[#FEE500] py-4'>
+        <button
+          type='button'
+          className='body1-bold text-text-primary flex w-full items-center justify-center gap-x-2 rounded-lg bg-[#FEE500] py-4'
+        >
           <Icon icon='KakaoLogo' />
           카카오톡으로 시작하기
         </button>
-        <button className='body1-bold border-line-400 flex w-full items-center justify-center gap-x-2 rounded-lg border py-4'>
+        <button
+          type='button'
+          className='body1-bold border-line-400 flex w-full items-center justify-center gap-x-2 rounded-lg border py-4'
+        >
           <Icon icon='GoogleLogo' />
           구글로 시작하기
         </button>
-        <button className='body1-bold text-text-primary-inverse flex w-full items-center justify-center gap-x-2 rounded-lg border bg-[#000000] py-4'>
+        <button
+          type='button'
+          className='body1-bold text-text-primary-inverse flex w-full items-center justify-center gap-x-2 rounded-lg border bg-[#000000] py-4'
+        >
           <Icon icon='AppleLogo' />
           Apple로 시작하기
         </button>
