755ec202f3d4fde320cdedc68329da183e9cc1b2
im-na0 <139189221+im-na0@users.noreply.github.com>
2025-07-14 23:20:39 +0900

    fix(knockdog): search modal view의 input으로 포커스 이동되게끔 수정
    - FloatingFocusManager를 사용하여 자식요소에서 focusable한 요소로 포커싱 가능하도록 수정
    - 포커스 트랩은 미구현

--- apps/knockdog/app/(home)/@modal/(...)search/page.tsx
+++ apps/knockdog/app/(home)/@modal/(...)search/page.tsx
@@ -1,6 +1,11 @@
 'use client';
 
 import { RemoveScroll } from 'react-remove-scroll';
+import {
+  FloatingFocusManager,
+  FloatingPortal,
+  useFloatingRootContext,
+} from '@floating-ui/react';
 import { SearchView } from '@features/search';
 
 // (home)/@modal/(...)search/page.tsx
@@ -8,9 +13,25 @@ import { SearchView } from '@features/search';
 //    Actual UI logic is managed by @features/search.
 //    This is because the search view is a modal and needs to be rendered in a modal.
 export default function Page() {
+  const context = useFloatingRootContext({
+    elements: {
+      reference: null,
+      floating: null,
+    },
+  });
   return (
-    <RemoveScroll className='absolute inset-0 z-[100]'>
-      <SearchView />
-    </RemoveScroll>
+    <FloatingPortal>
+      <FloatingFocusManager
+        context={context}
+        modal
+        initialFocus={0}
+        returnFocus
+        outsideElementsInert
+      >
+        <RemoveScroll className='absolute inset-0 z-[100]'>
+          <SearchView />
+        </RemoveScroll>
+      </FloatingFocusManager>
+    </FloatingPortal>
   );
 }
