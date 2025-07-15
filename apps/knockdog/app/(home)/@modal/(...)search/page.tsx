351d73dd919869348e1c88ba0a49e8780199a1e8
im-na0 <139189221+im-na0@users.noreply.github.com>
2025-07-15 15:31:21 +0900

    fix(knockdog): search modal focus trap 구현
    - input을 initialFocus로 설정
    - focus guards 설정(포커스 트랩 가능해짐)
    - outsideElementsInert 활성화로 외부 요소 차단

--- apps/knockdog/app/(home)/@modal/(...)search/page.tsx
+++ apps/knockdog/app/(home)/@modal/(...)search/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 
+import { useRef } from 'react';
 import { RemoveScroll } from 'react-remove-scroll';
 import {
   FloatingFocusManager,
   FloatingPortal,
-  useFloatingRootContext,
+  useFloating,
 } from '@floating-ui/react';
 import { SearchView } from '@features/search';
 
@@ -13,23 +14,29 @@ import { SearchView } from '@features/search';
 //    Actual UI logic is managed by @features/search.
 //    This is because the search view is a modal and needs to be rendered in a modal.
 export default function Page() {
-  const context = useFloatingRootContext({
-    elements: {
-      reference: null,
-      floating: null,
-    },
+  const { context, refs } = useFloating({
+    open: true,
+    onOpenChange: () => {},
   });
+
+  const searchInputRef = useRef<HTMLInputElement>(null);
+
   return (
     <FloatingPortal>
       <FloatingFocusManager
         context={context}
+        initialFocus={searchInputRef}
         modal
-        initialFocus={0}
+        guards
+        restoreFocus
         returnFocus
         outsideElementsInert
       >
-        <RemoveScroll className='absolute inset-0 z-[100]'>
-          <SearchView />
+        <RemoveScroll
+          ref={refs.setFloating}
+          className='absolute inset-0 z-[100]'
+        >
+          <SearchView inputRef={searchInputRef} />
         </RemoveScroll>
       </FloatingFocusManager>
     </FloatingPortal>
