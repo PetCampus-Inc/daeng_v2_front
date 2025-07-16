cd460c00226ef1c021c208c422e58bdb8404e78a
im-na0 <139189221+im-na0@users.noreply.github.com>
2025-07-16 15:17:18 +0900

    refactor(knockdog): placeMarker design token 사용하도록 변경

--- apps/knockdog/src/features/map/ui/PlaceMarker.tsx
+++ apps/knockdog/src/features/map/ui/PlaceMarker.tsx
@@ -10,7 +10,6 @@ interface PlaceMarkerProps {
   distance?: number;
   selected?: boolean;
   className?: string;
-  containerClassName?: string;
   onClick?: () => void;
 }
 
@@ -20,34 +19,30 @@ export function PlaceMarker({
   distance,
   selected,
   className,
-  containerClassName,
   onClick,
 }: PlaceMarkerProps) {
   return (
-    <div
-      className={cn('relative select-none', containerClassName)}
-      onClick={onClick}
-    >
+    <div className='relative select-none' onClick={onClick}>
       <div
         className={cn(
-          'flex min-w-[91px] flex-col items-center rounded-lg border border-[#41424A] bg-white p-2',
-          selected && 'bg-[#41424A]',
+          'border-line-700 bg-fill-secondary-0 radius-r2 p-x2 flex min-w-[91px] flex-col items-center border',
+          selected && 'bg-fill-secondary-700',
           className
         )}
       >
         <p
           className={cn(
-            'whitespace-nowrap text-sm font-bold text-[#15161B]',
-            selected && 'text-[#F3F3F7]'
+            'body2-bold text-text-primary whitespace-nowrap',
+            selected && 'text-text-primary-inverse'
           )}
         >
           {ellipsisText(title, titleMaxLength)}
         </p>
-        <div className='flex items-center gap-[2px]'>
+        <div className='gap-x0_5 flex items-center'>
           <span
             className={cn(
-              'text-xs text-[#8C8C94]',
-              selected && 'text-[#B4B4BB]'
+              'text-text-tertiary caption1-semibold',
+              selected && 'text-text-secondary-inverse'
             )}
           >
             {distance}km
@@ -55,18 +50,23 @@ export function PlaceMarker({
           <Icon
             icon='BookmarkFill'
             className={cn(
-              'size-4 text-[#15161B]',
-              selected && 'text-[#F3F3F7]'
+              'text-fill-secondary-700 size-x4',
+              selected && 'text-text-primary-inverse'
             )}
           />
           <Icon
             icon='Note'
-            className={cn('size-4', selected && 'text-[#F3F3F7]')}
+            className={cn(
+              'text-fill-secondary-700 size-x4',
+              selected && 'text-text-primary-inverse'
+            )}
           />
         </div>
       </div>
       <svg
-        className='absolute left-1/2 top-[calc(100%-8px)] -translate-x-1/2'
+        className={cn(
+          'absolute top-[calc(100%-8px)] left-1/2 -translate-x-1/2'
+        )}
         width='16'
         height='17'
         viewBox='0 0 16 17'
@@ -74,16 +74,21 @@ export function PlaceMarker({
         xmlns='http://www.w3.org/2000/svg'
       >
         <path
+          className={cn(
+            'stroke-fill-secondary-700 fill-fill-secondary-0',
+            selected && 'fill-fill-secondary-700'
+          )}
           d='M9.12012 13.751C8.66522 14.5797 7.52051 14.6315 6.97949 13.9062L6.87988 13.751L2.95508 6.60058C2.48775 5.7491 3.10396 4.70818 4.0752 4.70801L11.9248 4.70801C12.896 4.70818 13.5123 5.7491 13.0449 6.60059L9.12012 13.751Z'
-          fill={selected ? '#41424A' : '#FFF'}
-          stroke='#41424A'
         />
         <rect
+          className={cn(
+            'fill-fill-secondary-0',
+            selected && 'fill-fill-secondary-700'
+          )}
           x='2'
           y='4.00073'
           width='12'
           height='3'
-          fill={selected ? '#41424A' : '#FFF'}
         />
       </svg>
     </div>
