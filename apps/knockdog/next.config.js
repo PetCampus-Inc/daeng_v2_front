bd728f1df3422da5aaa6377450ca6036f286ab3f
yeomgahui <ykh4933@naver.com>
2025-08-06 10:11:13 +0900

    feat(ui): radio-component 생성

--- apps/knockdog/next.config.js
+++ apps/knockdog/next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  // API Proxy
+  async rewrites() {
+    return [
+      {
+        source: '/api/v0/:path*',
+        destination: 'https://api.knockdog.net/api/v0/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
