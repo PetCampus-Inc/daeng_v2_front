e22eb81860141dadb773793fa371bb8dae1d7684
im-na0 <139189221+im-na0@users.noreply.github.com>
2025-08-10 22:20:18 +0900

    feat(knockdog): dogschool list service 추가

---
+++ apps/knockdog/src/entities/dog-school/api/dogschool-list-service.ts
@@ -0,0 +1,19 @@
+import type { DogSchoolSearchListParams, DogSchoolSearchListResponse } from '../model/dogschool-list';
+import api from '@shared/api/ky-client';
+
+export abstract class DogSchoolListService {
+  static async getDogSchoolSearchList(params: DogSchoolSearchListParams) {
+    const searchParams = new URLSearchParams();
+    searchParams.set('refPoint', params.refPoint);
+    searchParams.set('bounds', params.bounds);
+    searchParams.set('zoomLevel', params.zoomLevel.toString());
+    searchParams.set('page', params.page?.toString() ?? '0');
+    searchParams.set('size', params.size?.toString() ?? '10');
+
+    return await api
+      .get<DogSchoolSearchListResponse>('kindergarten/map-view', {
+        searchParams,
+      })
+      .json();
+  }
+}
---
+++ apps/knockdog/src/entities/dog-school/model/dogschool-list.ts
@@ -0,0 +1,114 @@
+interface Coord {
+  lat: number;
+  lng: number;
+}
+
+interface PagingDto {
+  currentPage: number;
+  hasNext: boolean;
+  totalPage: number;
+}
+
+interface AggregationsDto {
+  sidoAggregations: SidoGunguAggregation[] | null;
+  sigunAggregations: SidoGunguAggregation[] | null;
+  geoBoundAggregation: GeoBoundAggregation;
+}
+
+interface SidoGunguAggregation {
+  code: string;
+  count: number;
+  label: string;
+  coord: Coord;
+  nextZoom: number;
+}
+
+interface GeoBoundAggregation {
+  sw: Coord;
+  ne: Coord;
+}
+
+interface DogSchoolResultDto {
+  totalCount: number;
+  list: DogSchool[];
+}
+
+export interface DogSchool {
+  id: string;
+  title: string;
+  ctg: string;
+  images?: string[];
+  operationTimes: {
+    startTime: string;
+    endTime: string;
+  };
+  operationStatus: keyof typeof OperationStatus;
+  price: number;
+  dist: number;
+  roadAddress: string;
+  coord: Coord;
+  reviewCount: number;
+  serviceTags: (keyof typeof ServiceTag)[];
+  pickupType: keyof typeof PickupType;
+}
+
+export const ServiceTag = {
+  DIARY: '1:1 알림장',
+  DOG_SHOP: '강아지 용품샵',
+  DOG_CAFE: '강아지 카페',
+  PARKING: '주차창',
+  VALET: '발렛파킹',
+
+  DAYCARE: '데이케어',
+  HOTEL: '호텔링',
+  STAY_24H: '24시간 상주',
+  TEMPERAMENT: '성격·성향 진단',
+  SPLIT_CLASS: '분반 돌봄',
+  BATH_SERVICE: '목욕',
+  WALK: '산책',
+  TRAINING: '훈련',
+  GROOMING: '미용',
+  REHABILITATION: '재활',
+
+  NON_SLIP: '미끄럼방지 바닥',
+  CCTV: 'CCTV',
+  PLAYGROUND: '놀이터',
+  ROOFTOP: '루프탑',
+  TERRACE: '테라스',
+  TRAINING_GROUND: '운동장',
+  YARD: '마당',
+} as const;
+
+export const PickupType = {
+  NONE: null,
+  FREE: '무료 픽드랍',
+  PAID: '유료 픽드랍',
+} as const;
+
+export const OperationStatus = {
+  OPEN: '영업중',
+  CLOSED: '영업종료',
+  DAY_OFF: '휴무',
+} as const;
+
+export interface DogSchoolSearchListResponse {
+  paging: PagingDto;
+  aggregations: AggregationsDto;
+  schoolResult: DogSchoolResultDto;
+}
+
+export type DogSchoolSearchListParams = {
+  refPoint: string;
+  bounds: string;
+  zoomLevel: number;
+  page?: number;
+  size?: number;
+};
+
+export type DogSchoolSearchListQueryParams = {
+  refPoint: { lat: number; lng: number };
+  bounds: naver.maps.LatLngBounds;
+  zoomLevel: number;
+  page?: number;
+  size?: number;
+};
