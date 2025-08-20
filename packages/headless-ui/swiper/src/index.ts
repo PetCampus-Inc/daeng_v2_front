947edb0ae9e5291002b250f93fd5e441ae1587c4
yeomgahui <ykh4933@naver.com>
2025-08-20 11:37:11 +0900

    feat(headless-ui): Swiper 컴포넌트 초기 인덱스 지원 및 코드 정리

--- packages/headless-ui/swiper/src/index.ts
+++ packages/headless-ui/swiper/src/index.ts
@@ -1,8 +1,3 @@
-export {
-  SwiperRoot,
-  SwiperSlideItem,
-  type SwiperRootProps,
-  type SwiperSlideItemProps,
-} from './swiper';
+export { SwiperRoot, SwiperSlideItem, type SwiperRootProps, type SwiperSlideItemProps } from './swiper';
 
 export * as Swiper from './swiper.namespace';
