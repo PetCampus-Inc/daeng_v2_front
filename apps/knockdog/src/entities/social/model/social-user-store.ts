c861d54bd9f3494c9dbe3a4759c99e5d630abf6d
SouthNova <jsn950509@gmail.com>
2025-08-21 22:56:57 +0900

    chore(knockdog): SocialUserStore 주석 추가

--- apps/knockdog/src/entities/social/model/social-user-store.ts
+++ apps/knockdog/src/entities/social/model/social-user-store.ts
@@ -3,7 +3,9 @@ import { create } from 'zustand';
 import { SocialUser } from './social-user';
 
 interface SocialUserStore {
+  /** 소셜 유저 정보 */
   socialUser: SocialUser | null;
+  /** 소셜 유저 정보 저장 */
   setSocialUser: (socialUser: SocialUser) => void;
 }
 
