3af7d2e3090cc669be3cf2afad84a9a44de561e4
SouthNova <jsn950509@gmail.com>
2025-09-07 05:37:13 +0900

    chore(knockdog): 루트 레이아웃 BottomNavBar 제거

--- apps/knockdog/app/layout.tsx
+++ apps/knockdog/app/layout.tsx
@@ -1,14 +1,15 @@
+import Script from 'next/script';
+
+import { NuqsAdapter } from 'nuqs/adapters/next/app';
+
 import { cn } from '@knockdog/ui/lib';
+import { NaverMapProvider } from '@knockdog/naver-map';
 
 import { suit } from './font';
 import './globals.css';
-import { NaverMapProvider } from '@knockdog/naver-map';
-import { NuqsAdapter } from 'nuqs/adapters/next/app';
-import Script from 'next/script';
 import { ReactQueryProvider } from '@app/providers/ReactQueryProvider';
 import { OverlayProvider } from '@app/providers/OverlayProvider';
 import { HeaderProvider, HeaderWrapper } from '@widgets/Header';
-import { BottomNavBar } from '@widgets/bottom-nav-bar';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -22,7 +23,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                   <div className='flex h-dvh flex-col'>
                     <HeaderWrapper />
                     {children}
-                    <BottomNavBar />
                   </div>
                 </HeaderProvider>
               </OverlayProvider>
