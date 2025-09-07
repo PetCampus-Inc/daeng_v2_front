07550754d71b47a2a514f723319049ffaeee1dc1
SouthNova <jsn950509@gmail.com>
2025-09-08 06:32:38 +0900

    chore(knockdog): 커밋 실수 파일 삭제

--- apps/knockdog/src/features/auth/index.ts
+++ apps/knockdog/src/features/auth/index.ts
@@ -1,6 +1,4 @@
 export { useLogin } from './model/useLogin';
 export { useVerificationTimer } from './model/useVerificationTimer';
 
-export { useVerifyEmail, VERIFICATION_STATUS } from './model/useVerifyEmail';
-
 export { LoginButton } from './ui/LoginButton';
