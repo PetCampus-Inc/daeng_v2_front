eb63a9be86613d84a6e19ccd5eb7f92eba194e51
SouthNova <jsn950509@gmail.com>
2025-09-08 07:32:41 +0900

    feat(knockdog): API 에러 코드에 UNKNOWN_ERROR 코드 추가

--- apps/knockdog/src/shared/api/model/constant/apiErrorCode.ts
+++ apps/knockdog/src/shared/api/model/constant/apiErrorCode.ts
@@ -1,6 +1,8 @@
 import { LOGIN_ERROR_CODE, TOKEN_ERROR_CODE } from './authErrorCode';
 
 const API_ERROR_CODE = {
+  /** 알 수 없는 에러 */
+  UNKNOWN_ERROR: 'UNKNOWN_ERROR',
   ...LOGIN_ERROR_CODE,
   ...TOKEN_ERROR_CODE,
 } as const;
