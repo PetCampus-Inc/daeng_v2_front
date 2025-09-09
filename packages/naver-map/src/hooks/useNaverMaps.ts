a400a5ad1c3e6658cd49c33d458a93000008c9f1
SouthNova <jsn950509@gmail.com>
2025-09-09 15:04:30 +0900

    fix(knockdog): 테스트

--- packages/naver-map/src/hooks/useNaverMaps.ts
+++ packages/naver-map/src/hooks/useNaverMaps.ts
@@ -7,11 +7,9 @@ export function useNaverMaps() {
     throw new Error('useNaverMaps는 클라이언트에서만 사용할 수 있습니다.');
   }
 
-  const clientId = process.env.NEXT_PUBLIC_NAVER_MAP_NCP_KEY;
+  const clientId = 's5hu0lc2kz'; //process.env.NEXT_PUBLIC_NAVER_MAP_NCP_KEY;
   if (!clientId) {
-    throw new Error(
-      "'NAVER_MAP_NCP_KEY' 값을 찾을 수 없습니다. .env 파일을 확인하세요."
-    );
+    throw new Error("'NAVER_MAP_NCP_KEY' 값을 찾을 수 없습니다. .env 파일을 확인하세요.");
   }
 
   return suspend(loadNavermapsScript, [clientId, 'naver-map']);
