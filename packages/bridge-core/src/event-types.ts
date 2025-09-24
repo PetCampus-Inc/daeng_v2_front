611451000a912f1a58bfc0162605723220112b71
yeomgahui <ykh4933@naver.com>
2025-09-24 12:57:51 +0900

    fix(bridge): 'device.getLatLng' 이벤트 타입 제거

--- packages/bridge-core/src/event-types.ts
+++ packages/bridge-core/src/event-types.ts
@@ -1,6 +1,5 @@
 interface BridgeEventMap {
   'bridge.ready': { nativeVersion: string; methods: string[] };
-  'device.getLatLng': { lat: number; lng: number };
 }
 
 export type { BridgeEventMap };
