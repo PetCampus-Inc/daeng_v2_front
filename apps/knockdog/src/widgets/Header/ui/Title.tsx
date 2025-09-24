a67f88bf14ff87fee16693c5b16aa97557cd1a7f
SouthNova <jsn950509@gmail.com>
2025-09-25 08:25:25 +0900

    refactor(knockdog): position prop 제거, 스타일링 개선

--- apps/knockdog/src/widgets/Header/ui/Title.tsx
+++ apps/knockdog/src/widgets/Header/ui/Title.tsx
@@ -2,27 +2,11 @@ import { ComponentProps } from 'react';
 import { cn } from '@knockdog/ui/lib';
 import { useHeaderContext } from '../model/HeaderProvider';
 
-function Title({
-  children,
-  position = 'center',
-  className,
-  ...props
-}: ComponentProps<'span'> & { position?: 'left' | 'center' | 'right' }) {
+function Title({ children, center = true, className, ...props }: ComponentProps<'span'> & { center?: boolean }) {
   const { textColor } = useHeaderContext();
 
   return (
-    <span
-      className={cn(
-        'text-[16px] font-bold',
-        {
-          'mx-auto': position === 'center',
-          'ml-auto': position === 'right',
-        },
-        textColor,
-        className
-      )}
-      {...props}
-    >
+    <span className={cn('h3-extrabold text-text-primary', textColor, center && 'mx-auto', className)} {...props}>
       {children}
     </span>
   );
