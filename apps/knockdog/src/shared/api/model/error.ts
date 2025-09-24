988be90a4800dd10681cf1dcebd262f52dbadf98
SouthNova <jsn950509@gmail.com>
2025-09-25 07:25:22 +0900

    refactor(knockdog): ApiError 클래스 코드 제네릭으로 변경

--- apps/knockdog/src/shared/api/model/error.ts
+++ apps/knockdog/src/shared/api/model/error.ts
@@ -2,10 +2,10 @@ import { API_ERROR_CODE } from './constant/apiErrorCode';
 
 type ApiErrorCode = (typeof API_ERROR_CODE)[keyof typeof API_ERROR_CODE];
 
-class ApiError extends Error {
+class ApiError<T = ApiErrorCode> extends Error {
   constructor(
     public status: number,
-    public code: ApiErrorCode,
+    public code: T,
     public message: string
   ) {
     super(message);
