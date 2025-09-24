b28165f9b7916d2289f2e767785f3853f7b24f9c
SouthNova <jsn950509@gmail.com>
2025-09-25 07:39:28 +0900

    refactor(knockdog): stack layout Header 컴포넌트 제거

--- apps/knockdog/app/(stack)/layout.tsx
+++ apps/knockdog/app/(stack)/layout.tsx
@@ -1,14 +1,8 @@
 'use client';
 
-import { Header, useHeaderContext } from '@widgets/Header';
-
 export default function AuthLayout({ children, modal }: { children: React.ReactNode; modal: React.ReactNode }) {
-  const { title } = useHeaderContext();
   return (
     <>
-      <Header>
-        <Header.Title>{title}</Header.Title>
-      </Header>
       {children}
       {modal}
     </>
