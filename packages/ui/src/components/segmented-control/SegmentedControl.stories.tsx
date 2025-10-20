b2d7609f3995dda4241ce66f41a2bce55c7a885c
im-na0 <139189221+im-na0@users.noreply.github.com>
2025-10-20 23:40:36 +0900

    chore: pnpm 업데이트 및 주요 패키지 업데이트 (#188)
    * build: bump bunchee from 5.1.5 to 6.6.2
    
    * build: pnpm 패키지매니저 버전업 8.5.6 -> 10.18.3
    
    * build(storybook,ui): migrate storybook v8 -> v9
    
    * build(deps-dev): bump various devDependencies to latest versions
    
    - Updated @commitlint/cli from 19.8.1 to 20.1.0
    - Updated @commitlint/config-conventional from 19.8.1 to 20.0.0
    - Updated @types/node from 22.15.18 to 24.7.2
    - Updated jsdom from 26.1.0 to 27.0.0
    - Updated prettier-plugin-tailwindcss from 0.6.14 to 0.7.0
    - Updated typescript from 5.8.3 to 5.9.3
    
    * build(deps): update React and React-DOM to version 19.2.0 across multiple packages
    
    - Removed peerDependencies for React and React-DOM in several package.json files.
    - Updated devDependencies to use React and React-DOM version 19.2.0.
    - Adjusted peerDependencies to require React and React-DOM version >=18.0.0 for compatibility.
    - Cleaned up package.json files in headless-ui components and other related packages.
    
    * chore(knockdog): ts 해석 시 react 로컬 타입으로 고정
    
    @see https://github.com/pnpm/pnpm/issues/7158
    
    * build(knockdog): naver-map 종속성 제거
    
    * build(deps): update React and TypeScript types to version 19.2.2 across multiple packages
    
    - Added @types/react and @types/react-dom as devDependencies in bridge-native, naver-map, and react-naver-map packages.
    - Updated react and react-dom to version 19.2.0 in respective package.json files.
    - Cleaned up pnpm-lock.yaml by removing unused dependencies and ensuring consistency across packages.
    
    * chore: Commitlint body max line length 제한없음으로 변경
    
    * chore(knockdog): next 빌드 시 추가되는 파일
    
    * chore(headless-ui): 타입검사 스크립트 제거
    
    * chore: workspace 수정

--- packages/ui/src/components/segmented-control/SegmentedControl.stories.tsx
+++ packages/ui/src/components/segmented-control/SegmentedControl.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/nextjs';
 import { SegmentedControl, SegmentedControlItem } from './SegmentedControl';
 
 const meta: Meta<typeof SegmentedControl> = {
