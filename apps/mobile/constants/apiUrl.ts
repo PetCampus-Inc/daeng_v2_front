3d300da1ecbae2771e644e3779f8ce0d27c9aae4
im-na0 <myblu1008@gmail.com>
2025-10-28 13:59:07 +0900

    build(mobile): env prefix 수정

--- apps/mobile/constants/apiUrl.ts
+++ apps/mobile/constants/apiUrl.ts
@@ -1,1 +1,1 @@
-export const API_URL = `${process.env.LOCAL_SERVER_IP}:${process.env.LOCAL_SERVER_PORT}`;
+export const API_URL = `${process.env.EXPO_PUBLIC_LOCAL_SERVER_IP}:${process.env.EXPO_PUBLIC_LOCAL_SERVER_PORT}`;
