08131a0eda9dff9b3b2e98ab5fb041b31158e9e8
im-na0 <myblu1008@gmail.com>
2025-11-06 19:49:06 +0900

    refactor(knockdog): 배럴 파일 정리

--- apps/knockdog/src/features/map/index.ts
+++ apps/knockdog/src/features/map/index.ts
@@ -1,7 +1,9 @@
-export * from './ui/MapView';
+export * from './ui/AggregationMarker';
 export * from './ui/CurrentLocationDisplayFAB';
 export * from './ui/CurrentLocationFAB';
 export * from './ui/ListFAB';
+export * from './ui/MapView';
+export * from './ui/PlaceMarker';
 export * from './ui/RefreshFAB';
 
 export { useNaverOpenRoute } from './model/useNaverOpenRoute';
