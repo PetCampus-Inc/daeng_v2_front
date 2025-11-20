da4b8895e4f56e6911143439a18e216247cf81df
SouthNova <jsn950509@gmail.com>
2025-11-21 00:33:05 +0900

    feat: 돌보는 강아지 여부 페이지 구현

---
+++ apps/knockdog/src/views/register/pet/ui/PetOwnershipPage.tsx
@@ -0,0 +1,32 @@
+import { ActionButton } from '@knockdog/ui';
+
+import { StackLink } from '@shared/lib/bridge';
+
+function PetOwnershipPage() {
+  return (
+    <div className='flex h-full flex-col'>
+      <div className='flex-1'>
+        <h1 className='h1-extrabold'>지금 돌보는 강아지가 있나요?</h1>
+
+        <div className='mx-auto mt-15 flex aspect-square w-[70%] items-center justify-center bg-gray-100'>
+          {/* 이미지 영역 */}
+        </div>
+      </div>
+
+      <div className='flex gap-2'>
+        <StackLink href='/register/user-nickname'>
+          <ActionButton variant='secondaryLine' size='large' className='w-full'>
+            아니오
+          </ActionButton>
+        </StackLink>
+        <StackLink href='/register/pet/profile'>
+          <ActionButton variant='secondaryFill' size='large' className='w-full'>
+            예
+          </ActionButton>
+        </StackLink>
+      </div>
+    </div>
+  );
+}
+
+export { PetOwnershipPage };
