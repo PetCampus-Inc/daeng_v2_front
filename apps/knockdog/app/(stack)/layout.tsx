f4771ec9319f9e7f81b88cfe888b650e873e35f0
SouthNova <jsn950509@gmail.com>
2025-11-23 00:16:47 +0900

    feat: 프로필 등록 및 반려동물 관련 페이지 레이아웃 추가

--- apps/knockdog/app/(stack)/layout.tsx
+++ apps/knockdog/app/(stack)/layout.tsx
@@ -1,5 +1,3 @@
-'use client';
-
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+export default function StackLayout({ children }: { children: React.ReactNode }) {
+  return <div className='h-full pt-[env(safe-area-inset-top)] pb-[env(safe-area-inset-bottom)]'>{children}</div>;
 }
