cdc0269c1ed0611ed4b77b84d1861fb8472e566b
im-na0 <myblu1008@gmail.com>
2025-11-26 00:44:05 +0900

    refactor(knockdog): BackButton 내부에서 Suspense 처리
    Header UI에 Suspense를 포함하는 것은 관심사 분리 측면에서 부적절하기 때문에 BackButton에서 Suspense 사용

--- apps/knockdog/src/widgets/Header/ui/BackButton.tsx
+++ apps/knockdog/src/widgets/Header/ui/BackButton.tsx
@@ -1,14 +1,16 @@
+'use client';
+
 import { Icon } from '@knockdog/ui';
 import { useHeaderContext } from '../model/HeaderProvider';
 import { cn } from '@knockdog/ui/lib';
-import type { ComponentProps } from 'react';
+import { Suspense, type ComponentProps } from 'react';
 import { useStackNavigation } from '@shared/lib/bridge';
 
 type BackButtonProps = ComponentProps<'button'> & {
   children?: string;
 };
 
-function BackButton({ children, onClick, ...props }: BackButtonProps) {
+function BackButtonInner({ children, onClick, ...props }: BackButtonProps) {
   const { back } = useStackNavigation();
   const { textColor } = useHeaderContext();
 
@@ -20,6 +22,14 @@ function BackButton({ children, onClick, ...props }: BackButtonProps) {
     <button className={cn('size-6', textColor)} onClick={onClick ?? handleBackClick} {...props}>
       {children ?? <Icon icon='ChevronLeft' />}
     </button>
+  );
+}
+
+function BackButton(props: BackButtonProps) {
+  return (
+    <Suspense fallback={<div className='size-6' />}>
+      <BackButtonInner {...props} />
+    </Suspense>
   );
 }
 
