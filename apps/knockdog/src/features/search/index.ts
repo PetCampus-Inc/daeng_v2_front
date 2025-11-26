afe37a8587f4c17cd1409546ba8e86524986f311
im-na0 <myblu1008@gmail.com>
2025-11-26 16:20:58 +0900

    revert(knockdog): revert: "feat(knockdog): 검색 query context 통해 관리"

--- apps/knockdog/src/features/search/index.ts
+++ apps/knockdog/src/features/search/index.ts
@@ -4,9 +4,6 @@ export { searchQueryOptions } from './api/searchQuery';
 /** config */
 export { FILTER_ICON_MAP } from './config/icon-map';
 
-/** model */
-export { useSearchQuery } from './model/useSearchQuery';
-
 /** ui */
 export { AutoCompleteList } from './ui/AutoCompleteList';
 export { RecentlyKeywordList } from './ui/RecentlyKeywordList';
