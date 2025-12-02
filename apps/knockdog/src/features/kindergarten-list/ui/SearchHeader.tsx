e65732c79ba319f8249c168c3c7ff21f56d5034d
im-na0 <myblu1008@gmail.com>
2025-12-02 19:50:46 +0900

    feat(knockdog): SearchHeader 컴포넌트 추가

---
+++ apps/knockdog/src/features/kindergarten-list/ui/SearchHeader.tsx
@@ -0,0 +1,61 @@
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Icon, TextField, TextFieldInput } from '@knockdog/ui';
+import { Header } from '@widgets/Header';
+import { useSafeAreaInsets } from '@shared/lib';
+
+interface SearchHeaderProps {
+  query: string;
+}
+
+export function SearchHeader({ query }: SearchHeaderProps) {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const { top } = useSafeAreaInsets();
+
+  /**
+   * 검색창 재활성화 (검색 페이지로 이동)
+   */
+  const handleGoSearch = () => {
+    const currentQuery = new URLSearchParams(searchParams.toString());
+    router.replace(`/search?${currentQuery.toString()}`);
+  };
+
+  /**
+   * 검색 취소 (메인 페이지로 이동)
+   */
+  const handleClose = () => {
+    const currentQuery = new URLSearchParams(searchParams.toString());
+    currentQuery.delete('query');
+    currentQuery.set('bottomSheetSnapIndex', '0');
+    router.replace(`/?${currentQuery.toString()}`);
+  };
+
+  return (
+    <Header
+      withSpacing={false}
+      className='bg-fill-secondary-0 absolute top-0 z-50 h-fit w-full'
+      style={{ paddingTop: top }}
+    >
+      <Header.LeftSection className='px-4'>
+        <Header.BackButton onClick={handleGoSearch} />
+      </Header.LeftSection>
+      <TextField
+        prefix={<Icon icon='Search' className='size-x6 text-fill-secondary-700' />}
+        className='bg-fill-secondary-50 h-x12 border-0'
+        onClick={handleGoSearch}
+      >
+        <TextFieldInput
+          type='search'
+          placeholder='업체 또는 주소를 검색하세요'
+          aria-label='검색어 입력'
+          autoFocus
+          value={query}
+          readOnly
+        />
+      </TextField>
+      <Header.RightSection className='px-4'>
+        <Header.CloseButton onClick={handleClose} />
+      </Header.RightSection>
+    </Header>
+  );
+}
