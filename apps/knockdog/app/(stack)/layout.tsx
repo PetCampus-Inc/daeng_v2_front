e450f162615e7af91d8d74092d4e9f960e712dff
SouthNova <jsn950509@gmail.com>
2025-12-07 17:19:48 +0900

    fix: 메인 레이아웃 SafeArea 모든 영역 활성화

--- apps/knockdog/app/(stack)/layout.tsx
+++ apps/knockdog/app/(stack)/layout.tsx
@@ -3,9 +3,5 @@
 import { SafeArea } from '@shared/ui/safe-area';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <SafeArea edges={['top']} className='flex h-dvh flex-col'>
-      {children}
-    </SafeArea>
-  );
+  return <SafeArea className='flex h-dvh flex-col'>{children}</SafeArea>;
 }
