060a689cd4d0ec4a8de480e5852c3e2508b83260
yeomgahui <ykh4933@naver.com>
2025-12-14 13:26:25 +0900

    refactor(knockdog): ReviewCard 컴포넌트에서 Avatar 사용 제거 및 img 태그로 대체

--- apps/knockdog/src/features/review/ui/ReviewCard.tsx
+++ apps/knockdog/src/features/review/ui/ReviewCard.tsx
@@ -1,15 +1,16 @@
-import Image from 'next/image';
 import type { Review } from '@entities/review';
-import { Avatar, AvatarImage, AvatarFallback } from '@knockdog/ui';
+// import { Avatar, AvatarImage, AvatarFallback } from '@knockdog/ui';
 
 export function ReviewCard({ username, profileImage, title, content, updatedAt }: Review) {
   return (
     <div className='bg-primitive-neutral-50 mb-2 flex flex-col gap-1 rounded-lg p-4'>
       <div className='flex items-center gap-1'>
-        <Avatar className='mr-1 inline-block size-6'>
+        {/* <Avatar className='mr-1 inline-block size-6'>
           <AvatarImage src={profileImage} alt='페이지 이미지' />
           <AvatarFallback>{username?.charAt(0) ?? 'KD'}</AvatarFallback>
-        </Avatar>
+        </Avatar> */}
+
+        <img src={profileImage} alt='페이지 이미지' className='mr-1 inline-block size-6 rounded-full' />
 
         <span className='body2-extrabold'>{username}</span>
       </div>
