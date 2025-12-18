ca60bf006bdd3d465b68c0bbe3bc962aad90d79a
im-na0 <myblu1008@gmail.com>
2025-12-18 16:11:15 +0900

    chore(mobile): 암호화 프롬프트 제외

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -28,6 +28,9 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     supportsTablet: true,
     usesAppleSignIn: true,
     bundleIdentifier: 'net.knockdog.petcampus.v2',
+    config: {
+      usesNonExemptEncryption: false,
+    },
     infoPlist: {
       LSApplicationQueriesSchemes: ['nmap', 'tel'],
       NSAppTransportSecurity: {
