2b086e054ee7769bc7d127bbbc983549c86734bd
JaneChun <108467211+JaneChun@users.noreply.github.com>
2026-01-09 14:33:57 +0900

    feat: 보관함 빈 상태 UI에 이미지 적용 (#346)
    * chore(knockdog): 북마크한 유치원이 없을 때 표시할 savednon 이미지 추가
    
    * feat(knockdog): 보관함 빈 상태 UI에 이미지 적용
    
    * refactor(knockdog): 유치원 탭 메인 페이지 이미지에 priority 속성 추가
    
    * refactor(knockdog): EmptySection은 조건부 렌더링되므로 이미지의 priority 속성 제거

--- apps/knockdog/src/widgets/save-tabs/ui/EmptySection.tsx
+++ apps/knockdog/src/widgets/save-tabs/ui/EmptySection.tsx
@@ -1,4 +1,5 @@
 import { ActionButton } from '@knockdog/ui';
+import Image from 'next/image';
 import { useTabNavigation } from '@shared/lib/bridge';
 
 function EmptySection() {
@@ -9,14 +10,21 @@ function EmptySection() {
   };
 
   return (
-    <div className='flex flex-1 flex-col items-center justify-center'>
-      <div className='mb-[112px] flex flex-col items-center'>
-        {/* 이미지 영역 */}
-        <div className='mb-5 h-40 w-40 bg-gray-200' />
+    <div className='flex flex-1 flex-col justify-between'>
+      <div className='mt-[70px] flex flex-col items-center'>
+        <Image
+          src='/images/img_savednon.png'
+          alt='저장된 유치원 없음'
+          className='mx-auto mb-5'
+          width={200}
+          height={200}
+        />
         <p className='h3-semibold'>아직 저장한 유치원이 없어요!</p>
       </div>
 
-      <ActionButton onClick={handleSearch}>유치원 탐색하기</ActionButton>
+      <div className='mb-12'>
+        <ActionButton onClick={handleSearch}>유치원 탐색하기</ActionButton>
+      </div>
     </div>
   );
 }
