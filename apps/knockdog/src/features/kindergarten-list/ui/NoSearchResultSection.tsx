538dc490a1b876190091ce45e23733071fc1c0f4
im-na0 <myblu1008@gmail.com>
2026-01-10 16:03:09 +0900

    fix(knockdog): 강아지 유치원 제보하기 버튼 ui 수정

--- apps/knockdog/src/features/kindergarten-list/ui/NoSearchResultSection.tsx
+++ apps/knockdog/src/features/kindergarten-list/ui/NoSearchResultSection.tsx
@@ -12,7 +12,7 @@ export function NoSearchResultSection() {
           </p>
         </div>
 
-        <ActionButton variant='secondaryFill' size='medium' disabled>
+        <ActionButton variant='secondaryFill' size='medium' className='w-auto' disabled>
           강아지 유치원 제보하기
         </ActionButton>
       </div>
