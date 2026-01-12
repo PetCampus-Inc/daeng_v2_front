362d6b7944c012de573fecb479e451275b88e5e2
im-na0 <139189221+im-na0@users.noreply.github.com>
2026-01-12 17:40:07 +0900

    feat: 탐색 검색결과 없음 UI 수정 (#359)
    * feat(knockdog): 탐색 검색결과 없음 이미지 추가
    
    * feat(knockdog): 강아지 유치원 제보하기 링크 추가
    
    * refactor(knockdog): Image priority 속성 제거

--- apps/knockdog/src/features/kindergarten-list/ui/NoSearchResultSection.tsx
+++ apps/knockdog/src/features/kindergarten-list/ui/NoSearchResultSection.tsx
@@ -1,10 +1,20 @@
 import { ActionButton } from '@knockdog/ui';
+import { EXTERNAL_LINKS } from '@shared/constants';
+import { useOpenExternalLink } from '@shared/lib/bridge';
+import Image from 'next/image';
 
 export function NoSearchResultSection() {
+  const openExternalLink = useOpenExternalLink();
+
+  const handleInformClick = () => {
+    openExternalLink(EXTERNAL_LINKS.INFORM);
+  };
+
   return (
     <div className='flex h-full w-full items-center'>
       <div className='px-x4 py-x8 flex w-full flex-col items-center justify-center'>
-        <div className='mb-[40px] text-center'>
+        <Image src='/images/img_no_result.png' alt='검색 결과 없음 이미지' width={200} height={200} />
+        <div className='mt-5 mb-10 text-center'>
           <p className='text-text-primary h3-semibold pb-x2'>검색 결과가 없어요</p>
           <p className='text-text-secondary body1-regular leading-relaxed'>
             똑독에 등록되지 않은 업체인가요? <br />
@@ -12,7 +22,7 @@ export function NoSearchResultSection() {
           </p>
         </div>
 
-        <ActionButton variant='secondaryFill' size='medium' className='w-auto' disabled>
+        <ActionButton variant='secondaryFill' size='medium' className='w-auto' onClick={handleInformClick}>
           강아지 유치원 제보하기
         </ActionButton>
       </div>
