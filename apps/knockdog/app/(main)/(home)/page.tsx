62895b92d90f537027db67bfda94e55eaa936e70
im-na0 <myblu1008@gmail.com>
2026-01-18 23:08:54 +0900

    feat(knockdog): globalcss에 web, webview variant 추가

--- apps/knockdog/app/(main)/(home)/page.tsx
+++ apps/knockdog/app/(main)/(home)/page.tsx
@@ -2,13 +2,11 @@
 
 import { Suspense } from 'react';
 import KindergartenMainPage from '@views/kindergarten-main-page';
-import { cn } from '@knockdog/ui/lib';
-import { isNativeWebView } from '@shared/lib';
 
 export default function Home() {
   return (
     <Suspense>
-      <div id='main' className={cn('relative h-full w-full', !isNativeWebView() && 'mb-(--bottom-bar-height)')}>
+      <div id='main' className='web:mb-(--bottom-bar-height) webview:mb-0 relative h-full w-full'>
         <KindergartenMainPage />
       </div>
     </Suspense>
