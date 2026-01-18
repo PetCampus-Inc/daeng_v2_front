f694a1cdb694818dda4aab21310eb22642ca23bc
im-na0 <myblu1008@gmail.com>
2026-01-18 22:28:34 +0900

    fix(knockdog): isNativeWebView를 함수로 호출하도록 변경

--- apps/knockdog/app/(main)/(home)/page.tsx
+++ apps/knockdog/app/(main)/(home)/page.tsx
@@ -1,20 +1,14 @@
 'use client';
 
-import { Suspense, useLayoutEffect, useState } from 'react';
+import { Suspense } from 'react';
 import KindergartenMainPage from '@views/kindergarten-main-page';
 import { cn } from '@knockdog/ui/lib';
 import { isNativeWebView } from '@shared/lib';
 
 export default function Home() {
-  const [isWebView, setIsWebView] = useState(false);
-
-  useLayoutEffect(() => {
-    setIsWebView(isNativeWebView());
-  }, []);
-
   return (
     <Suspense>
-      <div id='main' className={cn('relative h-full w-full', !isWebView && 'mb-(--bottom-bar-height)')}>
+      <div id='main' className={cn('relative h-full w-full', !isNativeWebView() && 'mb-(--bottom-bar-height)')}>
         <KindergartenMainPage />
       </div>
     </Suspense>
