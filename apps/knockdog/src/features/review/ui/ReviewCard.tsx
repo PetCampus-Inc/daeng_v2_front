9c223144d99991c0cc0e4a9e05c143392fbfaa52
yeomgahui <39826558+yeomgahui@users.noreply.github.com>
2026-01-19 14:39:28 +0900

    [Fix] 아이콘 및 tab index 오류 수정 (#389)
    * fix: 알림 다이얼로그 z-index 조정
    
    * feat: User, Kindergarten 아이콘 추가
    
    * feat(knockdog): ReviewCard 컴포넌트에서 Avatar 사용으로 프로필 이미지 표시 개선

--- apps/knockdog/src/features/review/ui/ReviewCard.tsx
+++ apps/knockdog/src/features/review/ui/ReviewCard.tsx
@@ -1,16 +1,16 @@
 import type { Review } from '@entities/review';
-// import { Avatar, AvatarImage, AvatarFallback } from '@knockdog/ui';
+import { Avatar, AvatarImage, AvatarFallback, Icon } from '@knockdog/ui';
 
 export function ReviewCard({ username, profileImage, title, content, updatedAt }: Review) {
   return (
     <div className='bg-primitive-neutral-50 mb-2 flex flex-col gap-1 rounded-lg p-4'>
       <div className='flex items-center gap-1'>
-        {/* <Avatar className='mr-1 inline-block size-6'>
-          <AvatarImage src={profileImage} alt='페이지 이미지' />
-          <AvatarFallback>{username?.charAt(0) ?? 'KD'}</AvatarFallback>
-        </Avatar> */}
-
-        <img src={profileImage} alt='페이지 이미지' className='mr-1 inline-block size-6 rounded-full' />
+        <Avatar className='mr-1 inline-block size-6'>
+          <AvatarImage src={profileImage} alt='프로필 이미지' />
+          <AvatarFallback className='bg-fill-secondary-200 p-[3px]'>
+            <Icon icon='User' className='size-6' />
+          </AvatarFallback>
+        </Avatar>
 
         <span className='body2-extrabold'>{username}</span>
       </div>
