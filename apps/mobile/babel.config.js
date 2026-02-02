72044971cf2f443fb5aa3c36b4b4004beacfa763
im-na0 <139189221+im-na0@users.noreply.github.com>
2026-02-02 20:52:49 +0900

    chore: 앱 설정 변경 (#417)
    * feat(knockdog): 네이버 지도 sdk 스크립트 https로 변경
    
    * build(mobile): 프로덕션 환경에서 console log 제거
    
    * chore(mobile): app config 설정 변경

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,9 +1,12 @@
 module.exports = function (api) {
   api.cache(true);
+  const isProduction = process.env.NODE_ENV === 'production';
+
   return {
     presets: ['babel-preset-expo'],
     plugins: [
       ['inline-dotenv', { path: '.env' }], // process.env.XXX 지원
+      ...(isProduction ? ['transform-remove-console'] : []),
     ],
   };
 };
