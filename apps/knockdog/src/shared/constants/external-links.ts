0ca0ec5cfc499d8a294a78b942b50e4fd05971f7
hyeonsu_ <117705439+agness4011@users.noreply.github.com>
2026-08-20 16:09:21 +0900

    chore: 약관 동의 노션 링크 수정

--- apps/knockdog/src/shared/constants/external-links.ts
+++ apps/knockdog/src/shared/constants/external-links.ts
@@ -1,7 +1,9 @@
 export const EXTERNAL_LINKS = {
   CONTACT: 'https://fifth-potato-175.notion.site/1-1-2ba6c15f67fb8080b956f7bea92a15d5?source=copy_link',
-  REQUIRED_TERMS_DETAIL:
-    'https://app.notion.com/p/2026-00-00-3856c15f67fb800f88f8e2e5bc38b427?source=copy_link',
+  /** 서비스 이용약관 */
+  TERMS_OF_SERVICE: 'https://app.notion.com/p/2026-08-27-3856c15f67fb800f88f8e2e5bc38b427',
+  /** 개인정보 수집/이용 동의 */
+  PRIVACY_POLICY: 'https://app.notion.com/p/2026-08-27-3856c15f67fb802eaba0de1c6b647c31',
   OWNER_VERIFICATION_CONTACT:
     'https://fifth-potato-175.notion.site/2fd6c15f67fb82e19f02814519824d5e?pvs=105',
   SUGGESTION: 'https://fifth-potato-175.notion.site/2ba6c15f67fb80088ff1e9d1d0293ca5?source=copy_link',
